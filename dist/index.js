59b721dfc200519fc46b9f5949e1c37c7b3ce333
Michal Dvorak <michal@mdvorak.org>
2020-06-09 12:44:42 +0200

    package

--- dist/index.js
+++ dist/index.js
@@ -407,9 +407,8 @@ async function run() {
 
         // Set outputs
         Object.keys(result).forEach(key => {
-            console.log("output", key, this[key]);
-            if (this[key] !== undefined) core.setOutput(key, this[key]);
-        }, result);
+            if (result[key] !== undefined) core.setOutput(key, result[key]);
+        });
     } catch (error) {
         core.setFailed(error.message);
     }
@@ -7213,7 +7212,7 @@ function nextVersion(base, tags) {
         base: baseVersion,
         last,
         next
-    }
+    };
 }
 
 module.exports = nextVersion;
